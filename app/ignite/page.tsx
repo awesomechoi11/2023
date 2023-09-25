036a0ec1646c8db8e6d44be2a6f5f608dc17fee9
awesomechoi11 <brandonchoi2016@gmail.com>
2023-09-24 17:35:19 -0700

    retrofit fadeindiv for fadeinimage

---
+++ app/ignite/page.tsx
@@ -0,0 +1,12 @@
+import HomeMain from "@/components/home/HomeMain";
+import Navbar from "@/components/navbar/Navbar";
+import MainCanvas from "@/components/three/MainCanvas";
+
+export default function Home() {
+  return (
+    <>
+      <HomeMain />
+      <Navbar />
+    </>
+  );
+}
